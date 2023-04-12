7cc61971a15c949b5f2f1bbe2108e7ac58f83470
okzfans <zhangfan@cloudtrek.cn>
2023-04-12 12:04:28 +0800

    fix: 去掉pwa将脚手架还原

--- bin/index.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-console.log("vue-okzfans-cli");
-require("../commander/index.js")
--- src/pwa/service-worker.js
+++
@@ -1,7 +0,0 @@
-export const unRegisterServiceWorker = () => {
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.ready.then(registration => {
-            registration.unregister()
-        })
-    }
-}
